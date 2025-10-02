0eb81df49e198ee82c74aa280fadcd7390185e3f
wzacarias <zacariaswilliams47@gmail.com>
2025-10-02 12:23:59 -0700

    Changed constraints, types, + more
    - Made separate transactions for organization.
    - Converted several attributes into foreign keys.
    - Added DELETE CASCADE where necessary.
    - Made genre, actor, and director name a colmun constraint.

--- table_set_up.sql
+++ table_set_up.sql
@@ -1,49 +1,57 @@
+
+-- Create initial entity tables --
+
+BEGIN;
+
 CREATE TABLE director (
     director_id SERIAL PRIMARY KEY,
-    director_name VARCHAR(24) NOT NULL
+    director_name VARCHAR(36) NOT NULL UNIQUE
 );
 
 CREATE TABLE actor (
     actor_id SERIAL PRIMARY KEY,
-    actor_name VARCHAR(24)
+    actor_name VARCHAR(36) NOT NULL UNIQUE
 );
 
 CREATE TABLE genre (
     genre_id SERIAL PRIMARY KEY,
-    genre_name VARCHAR(24) NOT NULL UNIQUE
+    genre_name VARCHAR(36) NOT NULL UNIQUE
 );
+
+COMMIT;
+
+-- Create movie relationships --
+
+BEGIN;
 
 CREATE TABLE movie (
     movie_id SERIAL PRIMARY KEY,
-    title VARCHAR(24) NOT NULL,
+    title VARCHAR(128) NOT NULL,
     release_date DATE,
-    duration_min TIME,
+    duration_min INT CHECK (duration_min > 0),
     director_id INT,
-    actor_id INT,
-    FOREIGN KEY (director_id) REFERENCES director(director_id),
-    FOREIGN KEY (actor_id) REFERENCES actor(actor_id)
+    FOREIGN KEY (director_id) REFERENCES director(director_id)
 );
 
 CREATE TABLE movie_genre (
-    movie_id INT,
-    genre_id INT,
-    PRIMARY KEY (movie_id, genre_id),
-    FOREIGN KEY (movie_id) REFERENCES movie(movie_id),
-    FOREIGN KEY (genre_id) REFERENCES genre(genre_id)
+    movie_id INT REFERENCES movie(movie_id) ON DELETE CASCADE,
+    genre_id INT REFERENCES genre(genre_id) ON DELETE CASCADE,
+    PRIMARY KEY (movie_id, genre_id)
 );
 
 CREATE TABLE movie_actor (
-    movie_id INT,
-    actor_id INT,
-    PRIMARY KEY (movie_id, actor_id),
-    FOREIGN KEY (movie_id) REFERENCES movie(movie_id),
-    FOREIGN KEY (actor_id) REFERENCES actor(actor_id)
+    movie_id INT REFERENCES movie(movie_id) ON DELETE CASCADE,
+    actor_id INT REFERENCES actor(actor_id) ON DELETE CASCADE,
+    character_name VARCHAR(128),
+    PRIMARY KEY (movie_id, actor_id)
 );
 
 CREATE TABLE watched (
     watched_id SERIAL PRIMARY KEY,
-    movie_id INT,
-    rating DECIMAL(2, 1),
-    notes TEXT,
-    FOREIGN KEY (movie_id) REFERENCES movie(movie_id)
+    movie_id INT REFERENCES movie(movie_id) ON DELETE CASCADE,
+    rating DECIMAL(3, 1) CHECK (rating >= 0 AND rating <= 10),
+    watched_date DATE DEFAULT CURRENT_DATE,
+    notes TEXT
 );
+
+COMMIT;
